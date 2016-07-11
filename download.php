ad7b446fb6208d890489a332527a403548c76651
Carlgo11 <github@carlgo11.com>
2016-07-11 23:27:49 +0200

    Make download.php both accept  &  vars

--- download.php
+++ download.php
@@ -1,7 +1,12 @@
 <?php
 
+function getVar($name){
+    if (isset($_GET[$name])) return $_GET[$name];
+    if (isset($_POST[$name])) return $_POST[$name];
+}
+
 include __DIR__ . '/res/API.php';
-$e = data_storage::getFile($_GET["f"], $_GET["p"]); # Returns [0] = File Meta Data, [1] = File Content.
+$e = data_storage::getFile(getVar("f"), getVar("p")); # Returns [0] = File Meta Data, [1] = File Content.
 $metadata = explode(" ", $e[0]); # Returns [0] = File Name, [1] = File Length, [2] = File Type.
 if (strpos($metadata[2], "image") != false) {
     header('Content-Description: File Transfer');
--- index.php
+++ index.php
@@ -61,7 +61,7 @@ $id = data_storage::checkUpload();
 
         <div id="download_file" style="float:right; width: 50%">
             <h2>Download File</h2>
-            <form class="form-horizontal" style="width: 30%;" action="download.php" method="GET" accept-charset="UTF-8" target="_blank" >
+            <form class="form-horizontal" style="width: 30%;" action="download.php" method="POST" accept-charset="UTF-8" target="_blank" >
                 <div class='form-group'><input class="form-control" type="text" name="f" id="f" required="" placeholder="ID"/></div>
                 <div class='form-group'><input class="form-control" type="password" name="p" id="p" required=""  placeholder="Password"/></div>
                 <button class="btn btn-lg btn-success" type="submit">Download File</button>
