1a11da16d37ffdc7111506e3426106b067862c5f
Carlgo11 <github@carlgo11.com>
2016-11-07 01:44:43 +0100

    Update error messages

--- api.php
+++ api.php
@@ -12,23 +12,31 @@ function output($success, $output) {
 
 $url = explode('/', strtolower($_SERVER['REQUEST_URI']));
 $success = false;
-if ($url[2] == 'upload') {
-  if ($_FILES['file'] != NULL) {
-    if (Misc::getVar('password') != NULL) {
-      $file = $_FILES['file'];
-      $password = Misc::getVar('password');
-      $id = data_storage::getID($file, $password);
-      if (is_bool($id[0]) && $id[0]) {
-        $url = 'https://tempfiles.carlgo11.com/download/' . $id[1] . '/?p=' . Misc::getVar('password');
-        $success=true;
-        print(output($success, ['url', $url]));
-      }else{
-        print(output($success, ['error', $id[1]]));
+if (count($url)) {
+  if ($url[2] == 'upload') {
+    if ($_FILES['file'] != NULL) {
+      if (is_file($_FILES['file'])) {
+        $file = $_FILES['file'];
+        if (Misc::getVar('password') != NULL) {
+          $password = Misc::getVar('password');
+          $id = data_storage::getID($file, $password);
+          if (is_bool($id[0]) && $id[0]) {
+            $url = 'https://tempfiles.carlgo11.com/download/' . $id[1] . '/?p=' . Misc::getVar('password');
+            $success = true;
+            print(output($success, ['url', $url . '.']));
+          } else {
+            print(output($success, ['error', $id[1]] . '.'));
+          }
+        } else {
+          print(output($success, ['error', 'No password.']));
+        }
+      } else {
+        print(output($success, ['error', 'The variable \'file\' is not a real file.']));
       }
     } else {
-      print(output($success, ['error', 'no password']));
+      print(output($success, ['error', 'No file.']));
     }
-  } else {
-    print(output($success, ['error', 'no file']));
   }
+} else {
+  print(output($success, ['error', 'Incorrectly formatted URL.']));
 }
