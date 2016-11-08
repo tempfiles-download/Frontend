6936b2b9af34279a94aad7b9c08fbaa7fe8fcdf5
Carlgo11 <github@carlgo11.com>
2016-11-08 17:54:40 +0100

    Fix file check error

--- api.php
+++ api.php
@@ -15,23 +15,19 @@ $success = false;
 if (count($url)) {
   if ($url[2] == 'upload') {
     if ($_FILES['file'] != NULL) {
-      if (is_file($_FILES['file'])) {
-        $file = $_FILES['file'];
-        if (Misc::getVar('password') != NULL) {
-          $password = Misc::getVar('password');
-          $id = data_storage::getID($file, $password);
-          if (is_bool($id[0]) && $id[0]) {
-            $url = 'https://tempfiles.carlgo11.com/download/' . $id[1] . '/?p=' . Misc::getVar('password');
-            $success = true;
-            print(output($success, ['url', $url . '.']));
-          } else {
-            print(output($success, ['error', $id[1]] . '.'));
-          }
+      $file = $_FILES['file'];
+      if (Misc::getVar('password') != NULL) {
+        $password = Misc::getVar('password');
+        $id = data_storage::getID($file, $password);
+        if (is_bool($id[0]) && $id[0]) {
+          $url = 'https://tempfiles.carlgo11.com/download/' . $id[1] . '/?p=' . Misc::getVar('password');
+          $success = true;
+          print(output($success, ['url', $url . '.']));
         } else {
-          print(output($success, ['error', 'No password.']));
+          print(output($success, ['error', $id[1]] . '.'));
         }
       } else {
-        print(output($success, ['error', 'The variable \'file\' is not a real file.']));
+        print(output($success, ['error', 'No password.']));
       }
     } else {
       print(output($success, ['error', 'No file.']));
