fd4090dd921a38d3fb747d04870a6640d2d81214
Carlgo11 <github@carlgo11.com>
2017-01-29 04:21:34 +0100

    Make IV exactly 16 bytes

--- res/API.php
+++ res/API.php
@@ -35,7 +35,6 @@ class data_storage {
   public static function uploadFile($content, $filename, $filesize, $filetype, $password) {
     include __DIR__ . '/config.php';
     $id = strtoupper(uniqid("d"));
-    $iv = array(base64_encode(openssl_random_pseudo_bytes(16)), base64_encode(openssl_random_pseudo_bytes(16)));
     $enc_filedata = base64_encode(Encryption::encrypt(implode(" ", array($filename, $filesize, $filetype)), $password, $iv[0]));
     $enc_content = base64_encode(Encryption::encrypt($content, $password, $iv[1]));
     $exportable_iv = base64_encode(implode(",", $iv));
