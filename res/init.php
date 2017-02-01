cbf798702bd68fc091f7c3f2b7c50da337d783cc
Carlgo11 <github@carlgo11.com>
2017-02-01 02:06:20 +0100

    Move mysql error to function

--- res/init.php
+++ res/init.php
@@ -1,5 +1,10 @@
 <?php
 
+function mySQLError() {
+  error_log('prepare() failed: ' . htmlspecialchars($con->error));
+  die("Connection to our database failed.");
+}
+
 if (file_exists(__DIR__ . '/config.php')) {
 
   $conf = include(__DIR__ . '/config.php');
@@ -11,4 +16,4 @@ if (file_exists(__DIR__ . '/API.php')) {
   include(__DIR__ . '/API.php');
 }
 
-$con = mysqli_connect($conf['mysql-url'], $conf['mysql-user'], $conf['mysql-password'], $conf['mysql-db']) or die("Connection problem.");
+$con = mysqli_connect($conf['mysql-url'], $conf['mysql-user'], $conf['mysql-password'], $conf['mysql-db']) or mySQLError();
