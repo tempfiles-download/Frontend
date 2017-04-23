f9f1ee1ad5eec88f3d178f5ba41b59b78249ad88
Carlgo11 <Carlgo11@users.noreply.github.com>
2017-04-23 02:08:45 +0200

    Fix headers for download_404.php

--- res/content/download_404.php
+++ res/content/download_404.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="res/css/bootstrap.min.css" rel="stylesheet">
-    <link href="res/css/tempfiles.css" rel="stylesheet" title="bright">
-    <link href="res/css/tempfiles-dark.css" rel="alternate stylesheet" title="dark">
+    <link href="/res/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/res/css/tempfiles.css" rel="stylesheet" title="bright">
+    <link href="/res/css/tempfiles-dark.css" rel="alternate stylesheet" title="dark">
     <?php
     if (isset($_POST['css'])) {
       echo "<link href=\"" . $_POST['css'] . "\" rel=\"stylesheet\">\n";
     }
     ?>
-    <script src="res/js/jquery.js"></script>
-    <script src="res/js/bootstrap.min.js"></script>
-    <script src="res/js/night-mode.js"></script>
+    <script src="/res/js/jquery.js"></script>
+    <script src="/res/js/bootstrap.min.js"></script>
+    <script src="/res/js/night-mode.js"></script>
     <title>TempFiles</title>
   </head>
 
