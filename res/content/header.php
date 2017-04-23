ed5049b58a1516119c98e8f908edc10bd7ed6b47
Edwin <git@olback.net>
2017-04-23 02:19:32 +0200

    Update header.php

--- res/content/header.php
+++ res/content/header.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="res/css/bootstrap.min.css" rel="stylesheet">
-    <link href="res/css/tempfiles.css" rel="stylesheet" title="bright">
-    <link href="res/css/tempfiles-dark.css" rel="alternate stylesheet" title="dark">
+    <link href="res/css/tempfiles.min.css" rel="stylesheet" title="bright">
+    <link href="res/css/tempfiles-dark.min.css" rel="alternate stylesheet" title="dark">
     <?php
     if (isset($_POST['css'])) {
       echo "<link href=\"" . $_POST['css'] . "\" rel=\"stylesheet\">\n";
@@ -14,6 +14,6 @@
     ?>
     <script src="res/js/jquery.js"></script>
     <script src="res/js/bootstrap.min.js"></script>
-    <script src="res/js/night-mode.js"></script>
+    <script src="res/js/night-mode.min.js"></script>
     <title>TempFiles</title>
   </head>
