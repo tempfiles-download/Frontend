c009977c183c57954af03bf5edf3ee68b1a5a7ae
Carlgo11 <Carlgo11@users.noreply.github.com>
2017-05-06 20:28:57 +0200

    Add creation of database and add maxviews row

--- install_mysql.sql
+++ install_mysql.sql
@@ -1,8 +1,11 @@
+CREATE DATABASE IF NOT EXISTS `tempfiles` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `tempfiles`;
 CREATE TABLE `files`(
   `id` VARCHAR(14) NOT NULL,
   `iv` VARCHAR(96) NOT NULL,
   `metadata` TEXT NOT NULL,
   `content` MEDIUMBLOB NOT NULL,
+  `maxviews` text,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY(`id`),
   UNIQUE KEY `id`(`id`)
