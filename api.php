41beaeb92bb72463382fd1c20a95d3fc17038072
Carlgo11 <Carlgo11@users.noreply.github.com>
2018-02-10 04:24:52 +0100

    Fix output bug when using upload API

--- api.php
+++ api.php
@@ -59,7 +59,7 @@ function upload($conf) {
                 $completeURL = $protocol . "://" . filter_input(INPUT_SERVER, 'HTTP_HOST') . "/download/" . $id[1] . "/?p=" . urlencode($password);
                 $output['success'] = true;
                 $output['url'] = $completeURL;
-                $output['id'] = $id;
+                $output['id'] = $id[1];
                 $output['deletepassword'] = $deletionpass;
                 if (Misc::getVar('password') == NULL) {
                     $output['passowrd-mode'] = 'Server generated.';
