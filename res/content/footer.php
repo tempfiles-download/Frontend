48701ff48116380484405a3ccd56ef3b80d9b814
Carlgo11 <Carlgo11@users.noreply.github.com>
2018-02-11 03:45:57 +0100

    Fix git commit version not showing This commit fixed a bug causing the commit version not showing due to PHP not having shell permissions.

--- res/content/footer.php
+++ res/content/footer.php
@@ -1,8 +1,10 @@
 <?php
 if ($conf['display-git-hash']) {
-  $hash = shell_exec('git rev-parse --short HEAD');
+//  $hash = shell_exec('git rev-parse --short HEAD');
+    $hashlong = file_get_contents('.git/refs/heads/master');
+    $hashshort = substr($hashlong, 0, 7);
 }
 ?>
 <footer>
-  <p>Made by <a href="https://carlgo11.com/">Carlgo11</a>. <?php if (!empty($hash)) { ?> Version: <a href="https://github.com/Carlgo11/tempfiles"><?php print($hash); ?></a><?php } ?></p>
+    <p>Made by <a href="https://carlgo11.com/">Carlgo11</a>. <?php if (!empty($hashshort)) { ?> Version: <a href="https://github.com/Carlgo11/tempfiles" title="<?php print($hashlong); ?>"><?php print($hashshort); ?></a><?php } ?></p>
 </footer>
