8243eb73cb4d079b559ceb1e8011ef49a9809c27
Carlgo11 <Carlgo11@users.noreply.github.com>
2018-08-01 04:16:38 +0200

    Remove move contents of /res out to main dir

--- frontend/js/night-mode.js
+++ frontend/js/night-mode.js
@@ -7,6 +7,6 @@ if (hour > 21 || hour < 8) {
 function switch_style() {
   var body = document.body;
   body.classList.add("dark");
-  $("#logo").attr("src", "res/img/logo.svg");
+  $("#logo").attr("src", "/img/logo.svg");
   $("#upload-password").addClass("dark");
 }
