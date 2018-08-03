99d12ea51833fddcc6ec55d55f7f9504f2acc317
Carlgo11 <Carlgo11@users.noreply.github.com>
2018-08-03 15:38:40 +0200

    Remove res/content folder

--- backend/res/content/navbar.php
+++
@@ -1,9 +0,0 @@
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="/"><img src="res/img/logo_light.svg" id="logo"></a><a class="navbar-brand nocolor" href="/">TempFiles</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-        </div><!--/.nav-collapse -->
-    </div>
-</nav>
