8d31e4d896154c01d0153e1bbc82b2fa1db44d1c
Carlgo11 <Carlgo11@users.noreply.github.com>
2018-08-04 21:45:39 +0200

    Set nightmode from 9pm to 8am

--- frontend/js/night-mode.js
+++ frontend/js/night-mode.js
@@ -1,12 +1,15 @@
-var d = new Date();
-var hour = d.getHours();
-if (hour > 21 || hour < 8) {
-  switch_style();
-}
+$(document).ready(function(e) {
+  var d = new Date();
+  var hour = d.getHours();
+  if (hour >= 21 || hour <= 8) {
+    switch_style();
+  }
 
-function switch_style() {
-  var body = document.body;
-  body.classList.add("dark");
-  $("#logo").attr("src", "/img/logo.svg");
-  $("#upload-password").addClass("dark");
-}
+  function switch_style() {
+    var body = document.body;
+    body.classList.add("dark");
+    $("#logo").attr("src", "/img/logo.svg");
+    $("#upload-password").addClass("dark");
+    $(".navbar").addClass("dark");
+  }
+});
