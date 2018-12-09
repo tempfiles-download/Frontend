35807851887dc4ac8330346d64f2655d1f76dda8
Carlgo11 <Carlgo11@users.noreply.github.com>
2018-12-09 05:12:00 +0100

    Update download() and remove compareViews() in favor of Misc.php::compareViews()

--- backend/API.php
+++ backend/API.php
@@ -7,12 +7,10 @@ header('Content-Type: application/json; charset=utf-8');
 set_error_handler('exceptions_error_handler');
 
 function exceptions_error_handler($severity, $message, $filename, $lineno) {
-    if (error_reporting() == 0) {
-        return;
-    }
-    if (error_reporting() & $severity) {
-        throw new ErrorException($message, 0, $severity, $filename, $lineno);
-    }
+    if (error_reporting() == 0) return;
+
+    if (error_reporting() & $severity)
+            throw new ErrorException($message, 0, $severity, $filename, $lineno);
 }
 
 /**
@@ -54,6 +52,7 @@ try {
 } catch (Exception $e) {
     $output['success'] = false;
     $output['error'] = $e->getMessage();
+    error_log($e);
     sendOutput($output);
 }
 
@@ -114,7 +113,7 @@ function upload($conf) {
             $output['password'] = $password;
 
             if ($file->getMaxViews() !== NULL) {
-                $output['maxviews'] = (int) $maxviews;
+                $output['maxviews'] = (int) $file->getMaxViews();
             }
         } else {
             throw new Exception($upload);
@@ -143,28 +142,25 @@ function delete() {
 /**
  * Gets the binary data for a file stored on the server.
  * @since 2.0
- * @todo Update to make compatible with v2.2 File handling.
  * 
  */
 function download() {
     require_once __DIR__ . '/res/ID.php';
 
-    $e = DataStorage::getFile(new ID(Misc::getVar("id")), Misc::getVar("p")); # Returns [0] = File Meta Data, [1] = File Content.
-    if ($e[0] != NULL) { //successful decryption
+    $file = DataStorage::getFile(new ID(Misc::getVar("id")), Misc::getVar("p"));
+    if ($file !== NULL) { //successful decryption
         $metadata = explode(" ", $e[0]); # Returns [0] = File Name, [1] = File Length, [2] = File Type, [3] = Deletion Password.
-        $output['success'] = true;
-        $output['data'] = base64_encode($e[1]);
-        $output['type'] = base64_decode($metadata[2]);
-        $output['filename'] = base64_decode($metadata[0]);
-        $output['length'] = base64_decode($metadata[1]);
-        $viewsArray = $e[1];
-        if (is_array($viewsArray)) {
-            compareViews($viewsArray[0], $viewsArray[1], $url[2]);
-            $output['currentviews'] = $viewsArray[0];
-            $output['maxviews'] = $viewsArray[1];
-        }
+        $output = [
+            "success" => true,
+            "type" => base64_decode($file->getMetaData('type')),
+            "filename" => base64_decode($file->getMetaData('name')),
+            "length" => base64_decode($file->getMetaData('size')),
+            "data" => $file->getContent()
+        ];
+
+        Misc::compareViews($file->getCurrentViews(), $file->getMaxViews(), $file->getID());
     } else {
-        $output['error'] = ">Bad ID or Password.";
+        $output['error'] = "Bad ID or Password.";
     }
     sendOutput($output);
     exit();
@@ -177,21 +173,4 @@ function download() {
 function cleanup() {
     $output['success'] = filter_var(DataStorage::deleteOldFiles(), FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
     sendOutput($output);
-}
-
-/**
- * Compare max views with current views.
- * @param int $currentviews Current views.
- * @param int $maxviews Maximum allowed views.
- * @param string $id ID.
- * @return boolean Returns true if current views surpass the maximum views. Otherwise returns false.
- * @since 2.1
- */
-function compareViews($currentviews, $maxviews, $id) {
-    if (($currentviews + 1) >= $maxviews) {
-        return DataStorage::deleteFile($id);
-    } else {
-        return DataStorage::setViews(intval($maxviews), ($currentviews + 1), $id);
-    }
-    return false;
 }
