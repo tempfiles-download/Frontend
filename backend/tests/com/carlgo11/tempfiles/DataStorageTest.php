053e16e29968d2fbb1bd9e96afdccd0a0836b051
Edwin Svensson <git@olback.net>
2019-05-29 20:30:37 +0200

    change use path

--- backend/tests/com/carlgo11/tempfiles/DataStorageTest.php
+++ backend/tests/com/carlgo11/tempfiles/DataStorageTest.php
@@ -2,7 +2,7 @@
 
 namespace com\carlgo11\tempfiles;
 
-use com\carlgo11\tempfiles\api\DataStorage;
+use com\carlgo11\tempfiles;
 use PHPUnit\Framework\TestCase;
 
 class DataStorageTest extends TestCase
