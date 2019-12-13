709c4be078c8de538aa112980a06be99044c7a4f
Carlgo11 <Carlgo11@users.noreply.github.com>
2019-12-13 16:46:30 +0100

    initialize bs custom file input in index.html only

--- js/version.js
+++ js/version.js
@@ -2,7 +2,6 @@
 ---
 
 $(document).ready(() => {
-    bsCustomFileInput.init();
     const longhash = '{{ site.github.build_revision }}';
     if (longhash.length > 0) {
         const shorthash = longhash.slice(0, 7);
