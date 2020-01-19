5154fd8eb31d44a7d3bee6bf16bc3db3fec53410
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-01-19 03:58:16 +0100

    Fix version.js in minified state

--- js/version.js
+++ js/version.js
@@ -1,7 +1,5 @@
 $(document).ready(() => {
     const longhash = '{{ site.github.build_revision }}';
-    if (longhash.length > 0) {
-        const shorthash = longhash.slice(0, 7);
-        $('#version').text(shorthash);
-    }
+    if (longhash.length > 0)
+        $('#version').text(longhash.slice(0, 7));
 });
