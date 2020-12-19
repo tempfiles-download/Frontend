7035b73424ea9d7bffcfd5c1852004e20494b2bf
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-12-19 21:09:55 +0100

    Fix deploy errors

--- _scripts/js-minify.sh
+++ _scripts/js-minify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in _site/js/*.js; do
+for file in js/*.js; do
   babel-minify "$file" -o "$file" --simplifyComparisons --simplify --mangle
   sed -i '1s/^/---\n---\n\n/' "$file"
 done
