a0fd1d414c7c15dd085cd93a102b1525bfe43732
Carl <Carlgo11@users.noreply.github.com>
2024-04-01 06:37:08 +0200

    Move encryption to local client (#10)
    * intial local-encryption implementation
    
    * Complete local encryption feature

--- js/upload.js
+++ js/upload.js
@@ -1,10 +1,24 @@
 const form = document.getElementById('upload-form')
-
 form.addEventListener("submit", async (e) => {
   e.preventDefault()
+  const formData = new FormData(form);
+
+  // Modify Form Data
+  const file = formData.get('file')
+  const encodedContent = btoa(String.fromCharCode(...new Uint8Array(await file.arrayBuffer())));
+  const password = formData.get('password') || generatePassword(6,20)
+  const data = await encryptData(JSON.stringify({
+    name: file.name,
+    type: file.type,
+    size: file.size,
+    data: encodedContent
+  }), password)
+  formData.set('file', data);
+
+  // Post formData to upload URL
   fetch('https://1.tmpfil.es/', {
     method: 'POST',
-    body: new FormData(form),
+    body: formData,
     cache: "no-cache"
   }).then(async (res) => {
     const data = await res.json()
@@ -12,8 +26,7 @@ form.addEventListener("submit", async (e) => {
         form.style.display = 'none'
         document.getElementById('title').innerHTML = 'File uploaded'
         document.getElementById('success').style.display = 'block'
-        document.getElementById('url').value = data.url
-        document.getElementById('server-password').value = data.password
+        document.getElementById('url').value = `${data.url}${password}`
         document.getElementById('deletion-password').value = data.deletepassword
       } else {
       if ('error' in data)
@@ -42,5 +55,16 @@ ondrop = (e) => {
 };
 
 window.onloadTurnstileCallback = () => {
-  turnstile.render('#captcha', {sitekey: '0x4AAAAAAAV3ZLwfyLkohb7z'});
+  turnstile.render('#captcha', {sitekey: '1x00000000000000000000BB'});
 };
+
+function generatePassword(minLength, maxLength) {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const length = Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+}
